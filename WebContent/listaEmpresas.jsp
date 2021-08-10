948113dd00ac4767f67baa2f297b804db8a927dd
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-10 14:39:59 -0300

    using jsp to /listaEmpresas

---
+++ WebContent/listaEmpresas.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.util.List, br.com.alura.gerenciador.servlet.Empresa" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Lista de Empresas</title>
+</head>
+<body>
+
+	<ul>
+	<%
+		List<Empresa> lista = (List<Empresa>)request.getAttribute("listaEmpresas");
+		for (Empresa empresa : lista) {
+	%>
+		<li><%= empresa.getNome() %></li>
+		
+	<% 
+		} 
+	%>
+	</ul>
+
+</body>
+</html>
--- src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,17 +20,11 @@ public class ListaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		
 		List<Empresa> lista = banco.getEmpresas();
-		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<ul>");
-		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</html></body>");
+	
+		//Chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		request.setAttribute("listaEmpresas", lista);
+		rd.forward(request, response);
 	}
 }
 
