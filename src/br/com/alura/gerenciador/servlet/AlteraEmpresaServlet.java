d60046363bf043fecd4042df3e7bd8f78fffd2d1
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-11 09:20:41 -0300

    implementing update servlet and creating the client-side redirect

--- src/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -1,33 +1,44 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AlteraEmpresaServlet
- */
 @WebServlet("/alteraEmpresa")
 public class AlteraEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String nomeEmpresa = request.getParameter("nome");
+		String paramId = request.getParameter("id");
+		String paramDataAbertura = request.getParameter("data");
+		Integer id = Integer.valueOf(paramId);
+		
+		Date dataAbertura = null;
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataAbertura);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		System.out.println(id);
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.buscaEmpresaPeloId(id);
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+		
+		response.sendRedirect("listaEmpresas");
 	}
 
 }
